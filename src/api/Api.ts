b6fdb9527be6334bf4228b3e2f84e02b0b02e8ef
Bogdan Negoita <negoitabogdan@icloud.com>
2025-07-13 13:24:27 +0300

    Specify method parameters and return types for Api and AuthService.

--- src/api/Api.ts
+++ src/api/Api.ts
@@ -1,80 +1,86 @@
+import type { AxiosInstance, AxiosRequestConfig } from 'axios';
 import axios from 'axios';
-import AuthService from './AuthService.ts';
+import AuthService, { type SignUpParams } from './AuthService.ts';
+
+export interface ApiParams extends AxiosRequestConfig {}
 
 export default class Api {
-	params = {};
-	authService = new AuthService(this);
+    params: ApiParams;
+    authService: AuthService;
+    caller: AxiosInstance;
 
-	constructor(params) {
-		const token = localStorage.getItem('token');
-		if (token) {
-			this.params = {
-				...params,
-				headers: { 'Authorization': 'Bearer ' + token },
-			}
-		} else {
-			this.params = params;
-		}
-		this.caller = axios.create(this.params);
-	}
+    constructor(params: ApiParams = {}) {
+        const token = localStorage.getItem('token');
+        if (token) {
+            this.params = {
+                ...params,
+                headers: { ...params.headers, 'Authorization': 'Bearer ' + token },
+            };
+        } else {
+            this.params = params;
+        }
+        this.caller = axios.create(this.params);
+        this.authService = new AuthService(this);
+    }
 
-	async logIn(email, password) {
-		const response = await this.authService.logIn(email, password);
-		const params = {
-			...this.params,
-			headers: { 'Authorization': 'Bearer ' + response.token },
-		};
-		this.caller = axios.create(params);
-	}
+    async logIn(email: string, password: string): Promise<void> {
+        const response = await this.authService.logIn(email, password);
+        const params = {
+            ...this.params,
+            headers: { ...this.params.headers, 'Authorization': 'Bearer ' + response.token },
+        };
+        this.caller = axios.create(params);
+    }
 
-	async signUp(user) {
-		await this.authService.signUp(user);
-		await this.logIn(user.email, user.password);
-	}
+    async signUp(user: SignUpParams): Promise<void> {
+        await this.authService.signUp(user);
+        await this.logIn(user.email, user.password);
+    }
 
-	logOut() {
-		this.authService.logOut();
-		this.params.headers = null;
-		this.caller = axios.create(this.params);
-	}
+    logOut(): void {
+        this.authService.logOut();
+        if (this.params.headers) {
+            this.params.headers = undefined;
+        }
+        this.caller = axios.create(this.params);
+    }
 
-	async get(resource, config = undefined, root = false) {
-		return await this.caller
-				.get(resource, config)
-				.then(res => root ? res : res.data);
-	}
+    async get<T = any>(resource: string, config?: AxiosRequestConfig, root = false): Promise<T> {
+        return await this.caller
+            .get(resource, config)
+            .then(res => (root ? res : res.data));
+    }
 
-	async post(resource, body = undefined, config = undefined) {
-		return await this.caller
-				.post(resource, body, config)
-				.then(res => res.data);
-	}
+    async post<T = any>(resource: string, body?: any, config?: AxiosRequestConfig): Promise<T> {
+        return await this.caller
+            .post<T>(resource, body, config)
+            .then(res => res.data);
+    }
 
-	async put(resource, body = undefined, config = undefined) {
-		return await this.caller
-				.put(resource, body, config)
-				.then(res => res.data);
-	}
+    async put<T = any>(resource: string, body?: any, config?: AxiosRequestConfig): Promise<T> {
+        return await this.caller
+            .put<T>(resource, body, config)
+            .then(res => res.data);
+    }
 
-	async delete(resource, config = undefined) {
-		return await this.caller
-				.delete(resource, config)
-				.then(res => res.data);
-	}
+    async delete<T = any>(resource: string, config?: AxiosRequestConfig): Promise<T> {
+        return await this.caller
+            .delete<T>(resource, config)
+            .then(res => res.data);
+    }
 
-	async upload(resource, file, config = {}) {
-		const formData = new FormData();
-		formData.append('file', file);
-		let requestConfig = { headers: { 'Content-Type': 'multipart/form-data' }, ...config }
-		return await this.caller
-				.post(resource, formData, requestConfig)
-				.then(res => res.data);
-	}
+    async upload<T = any>(resource: string, file: File, config: AxiosRequestConfig = {}): Promise<T> {
+        const formData = new FormData();
+        formData.append('file', file);
+        let requestConfig: AxiosRequestConfig = { headers: { 'Content-Type': 'multipart/form-data' }, ...config };
+        return await this.caller
+            .post<T>(resource, formData, requestConfig)
+            .then(res => res.data);
+    }
 
-	static errorTypes = {
-		entity_not_found: 'Something was not found :(',
-		user_not_found: 'An account with this email does not exist.',
-		email_used: 'An account with this email already exists.',
-	}
-
+    static errorTypes: Record<string, string> = {
+        entity_not_found: 'Something was not found :(',
+        user_not_found: 'An account with this email does not exist.',
+        email_used: 'An account with this email already exists.',
+    };
 }
--- src/api/AuthService.ts
+++ src/api/AuthService.ts
@@ -1,22 +1,31 @@
-export default class AuthService {
-	api;
+import type Api from "./Api";
 
-	constructor(api) {
+export interface SignUpParams {
+	email: string;
+	firstName: string;
+	lastName: string;
+	password: string;
+}
+
+export default class AuthService {
+	api: Api;
+
+	constructor(api: Api) {
 		this.api = api;
 	}
 
-	async logIn(email, password) {
+	async logIn(email: string, password: string): Promise<{ token: string }> {
 		localStorage.removeItem('token');
 		const response = await this.api.post('/login', { email, password });
 		localStorage.setItem('token', response.token);
 		return response;
 	}
 
-	async signUp({ email, firstName, lastName, password }) {
+	async signUp({ email, firstName, lastName, password }: SignUpParams): Promise<void> {
 		await this.api.post('/signup', { email, firstName, lastName, password });
 	}
 
-	logOut() {
+	logOut(): void {
 		localStorage.removeItem('token');
 	}
 }
