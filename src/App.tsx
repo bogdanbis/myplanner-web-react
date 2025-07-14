421d2467727e5bb2751b76a5c9b5492937055395
Bogdan Negoita <negoitabogdan@icloud.com>
2025-07-14 14:50:15 +0300

    Wait for User information before rendering content.

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { RouterProvider } from "react-router";
 import { useAppDispatch } from './store/hooks';
 import { fetchUser, logOut } from "./store/slices/authSlice";
 import { initPreferedColorScheme } from './store/slices/uiPreferencesSlice';
 import { router } from './router';
+import LoadingIcon from './components/ui/icons/LoadingIcon';
 
 export default function App() {
     const dispatch = useAppDispatch();
+    const [loading, setLoading] = useState(true);
 
     const logIn = async () => {
         if (localStorage.getItem('token')) {
@@ -20,10 +22,11 @@ export default function App() {
 
     useEffect(() => {
         dispatch(initPreferedColorScheme());
-        logIn();
+        logIn()
+            .then(() => setLoading(false));
     }, [dispatch]);
 
-    return (
-        <RouterProvider router={router} />
-    )
+    if (loading)
+        return <LoadingIcon />
+    return <RouterProvider router={router} />
 }
